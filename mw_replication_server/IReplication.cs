2e30d7572ca648293a0df0ae01687f8c86f4f2ef
marcelojcn <marcelojcnunes@hotmail.com>
2017-12-08 16:06:34 +0000

    Started replication

---
+++ mw_replication_server/IReplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mw_replication_server
+{
+    public interface IReplication
+    {
+        void ImAlive();
+
+        void replicateStates();
+    }
+}
---
+++ pacman_server/Replication.cs
@@ -0,0 +1,22 @@
+﻿using mw_replication_server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacman_server
+{
+    public class Replication : MarshalByRefObject, IReplication
+    {
+        public void ImAlive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void replicateStates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
